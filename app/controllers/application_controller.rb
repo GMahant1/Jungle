3f2d049135813acc33fa569bbe541e0f417bf552
Gaurav Mahant <thegau22@gmail.com>
2023-03-26 18:11:43 -0600

    define and implement simple authenticate for website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  before_action :authenticate
   
   protect_from_forgery with: :exception
 
@@ -29,4 +31,12 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
+  def authenticate 
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['USERNAME'] && password == ENV['PASSWORD']
+  end
+
+  end  
+
 end
